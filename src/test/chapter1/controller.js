352e8047d72f8bca202c5c33394a554a1655b342
hansilei <hzxshsl1@126.com>
2018-02-06 10:21:10 +0800

    Initial commit

---
+++ src/test/chapter1/controller.js
@@ -0,0 +1,6 @@
+var app = angular.module("myApp",[]);
+app.controller('HelloController',function ($scope) {
+    $scope.greeting = {
+        text:'Hello'
+    }
+})
---
+++ src/test/chapter2/controller.js
@@ -0,0 +1,60 @@
+//为核心的AMail服务创建模块
+var app = angular.module('AMail', []);
+
+//在URL、模块和控制器之间建立映射关系
+function emailRouteConfig($routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller: ListController,
+            templateUrl: 'list.html'
+        })
+        .when('/view/:id', {
+            controller: DetailController,
+            templateUrl: 'detail.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
+}
+
+app.config(emailRouteConfig);
+/*app.config('emailRouteConfig',function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller: ListController,
+            templateUrl: 'list.html'
+        })
+        .when('/view/:id', {
+            controller: DetailController,
+            templateUrl: 'detail.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
+})*/
+
+messages = [{
+    id: 0, sender: 'jean@somecompany.com', subject: 'Hi there, old friend',
+    date: 'Dec 7, 2013 12:32:00', recipients: ['greg@somecompany.com'],
+    message: 'Hey, we should get together for lunch sometime and catch up.'
+    + 'There are many things we should collaborate on this year.'
+}, {
+    id: 1, sender: 'maria@somecompany.com',
+    subject: 'Where did you leave my laptop?',
+    date: 'Dec 7, 2013 8:15:12', recipients: ['greg@somecompany.com'],
+    message: 'I thought you were going to put it in my desk drawer.'
+    + 'But it does not seem to be there.'
+}, {
+    id: 2, sender: 'bill@somecompany.com', subject: 'Lost python',
+    date: 'Dec 6, 2013 20:35:02', recipients: ['greg@somecompany.com'],
+    message: 'Nobody panic, but my pet python is missing from her cage.'
+    + 'She doesn\'t move too fast, so just call me if you see her.'
+}];
+
+var ListController = function ($scope) {
+    $scope.message = messages;
+}
+
+var DetailController = function ($scope, $routeParams) {
+    $scope.message = messages[$routeParams.id]
+}
